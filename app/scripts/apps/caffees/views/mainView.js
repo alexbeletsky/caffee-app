d39dd35f688d66698af0340e6aae65c5788cc875
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-09-13 14:56:07 +0300

    current version

--- app/scripts/apps/caffees/views/mainView.js
+++ app/scripts/apps/caffees/views/mainView.js
@@ -1,9 +1,25 @@
 define(function (require) {
     'use strict';
 
+    var Backbone = require('backbone');
+    var _ = require('underscore');
+
     var MainView = Backbone.View.extend({
+        template: '\
+            <ul class="caffes">\
+              <% _.forEach(caffes, function(caffe) { %>\
+                <li>\
+                    <img src="<%- caffe.image %>" />\
+                    <span class="name"><%- caffe.name %></span>\
+                    <span class="address"><%- caffe.address %></span>\
+                    <span class="city"><%- caffe.city %></span>\
+                </li><% }); %>\
+            </ul>\
+        ',
+
         render: function () {
-            this.$el.html('<h1>Caffees View</h1>');
+            var contents = _.template(this.template, {caffes: this.collection.toJSON()});
+            this.$el.html(contents);
         }
     });
 
